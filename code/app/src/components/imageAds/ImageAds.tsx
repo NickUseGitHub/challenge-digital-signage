a9eb2963f1844f04d9b6d33ebc2845d36f056abd
Chaiwtun <chaiwtun@scg.com>
2020-08-05 00:37:37 +0700

    [master] create component for ads

---
+++ code/app/src/components/imageAds/ImageAds.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './ImageAds.css'
+
+export default function ImageAds() {
+  return (
+    <div
+      className="image-ads"
+      style={{
+        backgroundImage:
+          "url('https://images.unsplash.com/photo-1596535293503-77f7cbb1f7e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80')",
+      }}
+    />
+  )
+}
