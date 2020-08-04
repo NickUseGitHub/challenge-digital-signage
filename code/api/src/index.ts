489f532b944a90f0b38b1fba1d1f9ca7af639603
Chaiwtun <chaiwtun@scg.com>
2020-08-04 22:56:19 +0700

    [master] fix script for watching mode

--- code/api/src/index.ts
+++ code/api/src/index.ts
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 
 app.get("/", function home(_, res: Response) {
-  res.send("Hello world")
+  res.send("Hello worlddd eoeo")
 })
 
 app.listen(port, function(err) {
